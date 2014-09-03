120462d893d086b7c656539aa7d4e4efd2f59eb8
Wuri Nugrahadi <wuri.nugrahadi@gmail.com>
2014-09-03 16:44:02 +0700

    partial theme liquid

---
+++ _site/docs/js/sample-editor.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1');
+    //bootstrap WYSIHTML5 - text editor
+    $(".textarea").wysihtml5();
+});
